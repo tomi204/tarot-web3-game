1c9ba534688cb3347edaa4a5ead4b7934d659196
Tomas Oliver <tomi204@outlook.com>
2024-11-24 15:22:50 -0300

    fix nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header: React.FC = () => {
 
   return (
     <>
-      <header className="top-0 left-0 w-full p-4 flex items-center bg-transparent z-50 ">
+      <header className="fixed top-0 left-0 w-full p-4 flex items-center bg-black/40 backdrop-blur-md z-50 ">
         <div className="absolute inset-x-0 top-1/2 transform -translate-y-1/2 text-center pointer-events-none">
           <h1 className="text-white font-bold text-xl">ALINEO</h1>
           <p className="text-white text-sm">FREE INTERACTIVE TAROT</p>
